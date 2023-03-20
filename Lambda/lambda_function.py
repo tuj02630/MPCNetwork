daa23bb2fbcaadaf3f783bf7f400b46ee9dea158
Keita Nakashima <tun05036@temple.edu>
2023-03-20 19:52:51 -0400

    Image Hosting on AWS Lambda with S3 Storage

--- Lambda/lambda_function.py
+++ Lambda/lambda_function.py
@@ -1,19 +1,91 @@
-import json
+import base64
 
+from mpc_api import MPC_API
+import boto3
+
+api = MPC_API()
+s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
     print(event)
     print(context)
     print("11")
-    data = "Event\n" + str(event) + "\nContext\n" + str(context)
-
-    return {
-        'statusCode': 200,
-        'headers': {'Content-Type': 'application/json', "x-api-key": "3hz1jabJZ08Ee9ZsTzGEc9NlkLM0U5uF7inDPeoW"},
-        'body': json.dumps(data)
+    data = {
+        "event": event,
+        "content": context
     }
 
-# app = Flask(__name__)
-# @app.route('/', methods=['GET'])
-# def home():
-#     return jsonify({'message': 'Hello, World!'})
+    status = 200
+
+    try:
+        if event["queryStringParameters"] is not None and "event_type" in event["queryStringParameters"]:
+            if event["queryStringParameters"]["event_type"] in api.handlers:
+                return api.handlers[event["queryStringParameters"]["event_type"]](event["queryStringParameters"])
+            else:
+                status = 500
+                data = {"error": "Error Key " + event["queryStringParameters"]["event_type"]}
+    except Exception as err:
+        status = 500
+        data = {"error": str(err)}
+
+    return {
+        'statusCode': status,
+        'headers': {'Content-Type': 'application/json'},
+        'body': str(data)
+    }
+
+
+@api.handle("Video")
+def video_request(para):
+    if "image_name" in para:
+        image_name = para["image_name"]
+    else:
+        image_name = "image.png"
+    response = s3.get_object(
+        Bucket='mpc-capstone',
+        Key=image_name,
+    )
+    if image_name[-4:] == ".png":
+        content_type = "image/png"
+    elif image_name[-4:] == ".jpg" or image_name[-4:] == ".jpeg":
+        content_type = "image/jpeg"
+    else:
+        raise ValueError("Invalid image name: " + image_name)
+    image = response['Body'].read()
+    print(base64.b64encode(image).decode('utf-8'))
+    return {
+        'headers': {"Content-Type": content_type},
+        'statusCode': 200,
+        'body': base64.b64encode(image).decode('utf-8'),
+        'isBase64Encoded': True
+    }
+    # return {
+    #     'statusCode': 200,
+    #     'headers': {'Content-Type': 'text/plain'},
+    #     'body': "Video"
+    # }
+
+
+@api.handle("Account")
+def account_request(para):
+    return {
+        'statusCode': 200,
+        'headers': {'Content-Type': 'text/plain'},
+        'body': "Account"
+    }
+
+
+@api.handle("Recording")
+def recording_request(para):
+    return {
+        'statusCode': 200,
+        'headers': {'Content-Type': 'text/plain'},
+        'body': "Recording"
+    }
+
+
+if __name__ == "__main__":
+    api.handlers["Message"](None)
+    api.handlers["Post"](None)
+    api.handlers["Message"](None)
+
--- Lambda/mpc_api.py
+++ Lambda/mpc_api.py
@@ -7,39 +7,12 @@ class MPC_API:
 
     def handle(self, action):
         def decorator(func):
-            print("inside")
+            # print("inside")
             self.add_handler(func, action)
             return func
         return decorator
 
     def add_handler(self, fun, action):
         self.handlers[self.get_key(action)] = fun
-
-
-api = MPC_API()
-
-
-@api.handle("Message")
-def function(event):
-    print("Message")
-    return event
-
-
-@api.handle("Post")
-def function(event):
-    print("Post")
-    return event
-
-
-@api.handle("GET")
-def function(event):
-    print("GET")
-    return event
-
-
-if __name__ == "__main__":
-    api.handlers["Message"](None)
-    api.handlers["Post"](None)
-    api.handlers["Message"](None)
 
 
